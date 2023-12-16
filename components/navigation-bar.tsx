b46f5dbdb14cd3ad32f79172b66b98e92049f152
Erny Salcedo <ernydavid04@gmail.com>
2023-12-16 12:05:57 -0500

    adding logo

--- components/navigation-bar.tsx
+++ components/navigation-bar.tsx
@@ -12,8 +12,12 @@ export default async function Navbar () {
   return (
     <header className='w-full flex items-center justify-center px-6 xl:px-4 bg-background/80 backdrop-blur-md group-[data]:backdrop-blur-md fixed top-0 left-0 border-b border-b-border/50'>
       <div className='w-full max-w-7xl h-20 flex items-center justify-between'>
-        <Link href='/'>
-          Logo
+        <Link
+          className='flex'
+          href='/'
+        >
+          <span>Logo</span>
+          <span className='font-bold'>App</span>
         </Link>
 
         <div className='flex items-center gap-3'>
